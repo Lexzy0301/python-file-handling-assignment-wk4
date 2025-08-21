d633f144f70a09cb91bebdb9036588cc344ff438
Arioye Hammed Olamilekan <adekunleatoba783@hotmail.com>
2025-08-21 01:55:10 -0500

    file_read.py

--- file_read.py
+++ file_read.py
@@ -1,10 +1,10 @@
 #open new txt file and write
 file = open('concept.txt', 'w')
-file.write('this is lexzy concept/n')
-file.write('a fashion brand/n')
-file.write('we deal fashion wear for men/n')
-file.write('we also style/n')
-file.write('you can check us out on IG at Lexzyconcept/n')
+file.write("this is lexzy concept\n")
+file.write("a fashion brand\n")
+file.write("we deal fashion wear for men\n")
+file.write("we also style\n")
+file.write("you can check us out on IG at Lexzyconcept\n")
 
 #read txt file
 file = open('concept.txt', 'r')
@@ -23,3 +23,4 @@ file.write(data_upper + "\n")
 file.write(f"\nWord Count: {word_count}\n")
 
 print("file 'twl.txt'created successfully")
+
